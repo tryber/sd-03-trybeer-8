e3de0653100f09b9b470b56c22af4d192176ab2b
luizfernando <luizfernandotic@gmail.com>
2020-09-23 16:35:09 -0300

    adjust port to 3001

--- back-end/index.js
+++ back-end/index.js
@@ -11,6 +11,6 @@ app.use('/register', userController);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
