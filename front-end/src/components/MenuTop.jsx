128640967dd0c1467d1a0bf21e434106a8c8732f
Henrique Rezende <henriquerezendec@gmail.com>
2020-09-24 19:20:07 -0300

    Eslint

--- front-end/src/components/MenuTop.jsx
+++ front-end/src/components/MenuTop.jsx
@@ -1,26 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../style/MenuTop.css';
 
 const MenuTop = ({ title }) => (
-    <div className="menu-top">
-      <h1 className="app-name" data-testid="top-title">
-        { title }
-      </h1>
+  <div className="menu-top">
+    <h1 className="app-name" data-testid="top-title">
+      {title}
+    </h1>
+    <label htmlFor="hamburguer-input">
       <input id="hamburguer-input" className="hamburguer-input" type="checkbox" />
-      <label htmlFor="hamburguer-input">
-        <div className="menu">
-          <span className="hamburguer"></span>
-        </div>
-      </label>
+      <div className="menu">
+        <span className="hamburguer" />
+      </div>
+    </label>
 
-      <ul className="btn-list">
-        <li><button type="button" className="menu-btn">Produtos</button></li>
-        <li><button type="button" className="menu-btn">Meus Pedidos</button></li>
-        <li><button type="button" className="menu-btn">Meu Perfil</button></li>
-        <li><button type="button" className="menu-btn">Sair</button></li>
-      </ul>
-    </div>
-  );
+    <ul className="btn-list">
+      <li>
+        <button type="button" className="menu-btn">
+          Produtos
+        </button>
+      </li>
+      <li>
+        <button type="button" className="menu-btn">
+          Meus Pedidos
+        </button>
+      </li>
+      <li>
+        <button type="button" className="menu-btn">
+          Meu Perfil
+        </button>
+      </li>
+      <li>
+        <button type="button" className="menu-btn">
+          Sair
+        </button>
+      </li>
+    </ul>
+  </div>
+);
 
+MenuTop.propTypes = {
+  title: PropTypes.string.isRequired,
+};
 
 export default MenuTop;
