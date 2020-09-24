bf3c9cc922353941b1372acbe888a75ab11506ae
rdgbrezolin <brezolinr@gmail.com>
2020-09-24 18:07:25 -0300

    page register with css

---
+++ front-end/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <h1 className="text-center">Trybeer - Grupo 8</h1>
+    </div>
+  );
+}
+
+export default HomePage;
