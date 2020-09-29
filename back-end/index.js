e88d7c0f0b4ec06f4efc3fc82df35ac90d61562e
tales <talestales@gmail.com>
2020-09-29 09:29:36 -0300

    order

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
-const { userController, productController, loginController, profileController, saleController } = require('./controller');
+const { userController, productController, loginController, profileController, saleController, orderController } = require('./controller');
 const { errorHandler } = require('./middlewares/errorHandle');
 
 const app = express();
@@ -16,6 +16,7 @@ app.use('/register', userController);
 app.use('/login', loginController);
 app.use('/profile', profileController);
 app.use('/checkout', saleController);
+app.use('/orders', orderController);
 
 app.use(errorHandler);
 
