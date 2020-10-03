a1e4c3630f098356b4c5d61c6494cd9fccf49ec6
Henrique Rezende <henriquerezendec@gmail.com>
2020-10-03 13:51:45 -0300

    lint

--- back-end/middlewares/errorHandle.js
+++ back-end/middlewares/errorHandle.js
@@ -1,13 +1,10 @@
 const errorHandler = (error, _req, res, _next) => {
-
   if (error.info && error.info.code === 1062) {
     return res.status(409).json({ message: error.message });
   }
-
   if (error.message) {
     return res.status(400).json({ message: error.message });
   }
-
   return res.status(500).json({ message: error.message, stack: error.stack });
 };
 
